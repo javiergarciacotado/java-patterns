2ecebf533bc64f6e758628ae6fada2c290dbdc47
Javier Garcia Cotado <javier.garcia_cotado@roche.com>
2022-03-17 08:49:59 +0100

    Improves isp responsability

--- 01-design-principles/src/main/java/com/owned/design/principles/solid/interface_segregation/weird/example_1/BankAccountNotifications.java
+++ 01-design-principles/src/main/java/com/owned/design/principles/solid/interface_segregation/weird/example_1/BankAccountNotifications.java
@@ -1,5 +1,6 @@
 package com.owned.design.principles.solid.interface_segregation.weird.example_1;
 
+//TODO 16/03/22 - Apply pattern correctly
 public interface BankAccountNotifications {
 
     void notify(String channel, String message); //slack
