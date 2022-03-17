8b3477f57509754a7b881af9d0e775d267f1466b
Javier Garcia Cotado <javier.garcia_cotado@roche.com>
2022-03-17 08:44:49 +0100

    Improves singleton test

--- 01-design-principles/src/test/java/com/owned/design/principles/stupid/weird/singleton/JdbcConnectionV2Should.java
+++ 01-design-principles/src/test/java/com/owned/design/principles/stupid/weird/singleton/JdbcConnectionV2Should.java
@@ -3,17 +3,23 @@ package com.owned.design.principles.stupid.weird.singleton;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class JdbcConnectionV2Should {
 
     //problem starts here ....
     private final Connection sut = JdbcConnectionV2.getInstance();
 
+    private Connection jdbcConnectionV2;
+
     @BeforeEach
     void setUp() {
+        //Do you see it?
+        jdbcConnectionV2 = JdbcConnectionV2.getInstance();
     }
 
     @Test
     void whatever_test() {
-
+        assertThat(1).isEqualTo(1);
     }
 }
